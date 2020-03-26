2641ba1078423fe3f03951c3317fdbaf31775ffd
Athly23 <andrewly06@gmail.com>
2020-03-26 14:39:00 -0400

    made image and ingredients

--- p4.js
+++ p4.js
@@ -7,21 +7,53 @@
 
 
 
-let drinks = {
-    'bloody-mary': {
-        'drink-url': "./drinkimg/bloody.jpeg",
+let drinks = [
+     {
+        'drink-url': "drinkimg/bloody.jpeg",
         "contents": ["1 and a half ounces of vodka", "3 ounces of tomato juice", "2 dashes of worcestershire", "3 dashes of tobasco", "garnish with celery"],
-        'name-of-drink': 'Blood Mary'
+        'name-of-drink': 'Bloody Mary'
     },
-    'cosmopolitan': {
-        'drink-url': "./drinkimg/cosmo.jeg",
+    {
+        'drink-url': "drinkimg/cosmo.jpeg",
         "contents": ["1 and a half ounces of vodka", "2 ounces of cranberry juice", "squeeze 2 lime wedges"],
         "name-of-drink": "cosmopolitan"
     },
-    'whiskey-sour': {
-        'drink-url': "./drinkimg/images.jpeg",
+    {
+        'drink-url': "drinkimg/images.jpeg",
         "contents": ["2 ounces of whiskey", "2 ounces of homemade sour mix", "garnish with cherry"],
-        "name-of-drink": "whiskey-sour"
+        "name-of-drink": "whiskey sour"
     }
+]
+
+let score = 0; 
+
+// for (let i = 0; i < drinks.length; i++) this for loop is for the drinks variable for the object within
+
+// drinks.forEach(drinksContent)
+
+// function drinkContent(item)
+// document.querySelectorAll("#question-list").textContent + item;
+
+// let questionList = document.querySelectorAll("#question-list")
+// for (let i = 0; i < questionList.length; i++) {
+
+// }
+let ingredients = document.querySelector(".ingredients")
+let pass = 0
+let drinkPics = document.querySelector(".cocktail-pic")
+function play() {
+    // drinks[0]["bloody-mary"]["drink-url"]
+    console.log(drinks[0]["drink-url"])
+    drinkPics.setAttribute("src", drinks[0]["drink-url"])
+    drinks[0]["contents"].forEach(drinkContent => {
+        ingredients.innerHTML += `<p> ${drinkContent} </p>`; 
+    })
+
 }
+
+let playButton = document.querySelector(".play-button")
+playButton.addEventListener("click", play)
+
+
+
 
