216ce878958c8e7545834cdec8d8e3e8a5f04cd2
Athly23 <andrewly06@gmail.com>
2020-03-25 21:54:51 -0400

    first commit base layout

---
+++ p4.js
@@ -0,0 +1,27 @@
+//make a button/submit on the main page to access the trivia "using slider ui?"
+//make an eventlistener for the "submit" for when the user types the cocktail name
+//make "if" "else" statements for the correct image and incorrect image
+// once clicked the user know if they are "correct" or "incorrect" 
+//then it will move to the next slide  
+//the score will be tracked and kept above the questions on all slides.
+
+
+
+let drinks = {
+    'bloody-mary': {
+        'drink-url': "./drinkimg/bloody.jpeg",
+        "contents": ["1 and a half ounces of vodka", "3 ounces of tomato juice", "2 dashes of worcestershire", "3 dashes of tobasco", "garnish with celery"],
+        'name-of-drink': 'Blood Mary'
+    },
+    'cosmopolitan': {
+        'drink-url': "./drinkimg/cosmo.jeg",
+        "contents": ["1 and a half ounces of vodka", "2 ounces of cranberry juice", "squeeze 2 lime wedges"],
+        "name-of-drink": "cosmopolitan"
+    },
+    'whiskey-sour': {
+        'drink-url': "./drinkimg/images.jpeg",
+        "contents": ["2 ounces of whiskey", "2 ounces of homemade sour mix", "garnish with cherry"],
+        "name-of-drink": "whiskey-sour"
+    }
+}
+
